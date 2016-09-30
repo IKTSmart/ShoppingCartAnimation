d511ffdb4c3533f1e870f8cd33a5c23aab6b7c2e
IKTSmart <a5786117@qq.com>
2016-09-30 14:42:32 +0800

    ShoppingCartAnimation

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoppingCartAnimation
+//
+//  Created by IKT on 16/9/30.
+//  Copyright © 2016年 www.tuanmob.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,78 @@
+//
+//  ViewController.m
+//  ShoppingCartAnimation
+//
+//  Created by IKT on 16/9/30.
+//  Copyright © 2016年 www.tuanmob.com. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ParabolaView.h"
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableIVew;
+@property (nonatomic, strong) NSArray *data;
+
+@end
+
+@implementation ViewController{
+    CGPoint _endP;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    _tableIVew = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-50)];
+    [_tableIVew setDataSource:self];
+    [_tableIVew setDelegate:self];
+    [_tableIVew registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cellId"];
+    [self.view addSubview:_tableIVew];
+    
+    UIView *cart = [[UIView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height-50, 50, 50)];
+    [cart setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:cart];
+    _endP = CGPointMake(cart.center.x, cart.frame.origin.y);
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 60;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellId" forIndexPath:indexPath];
+    [cell.textLabel setText:@"商品"];
+    UIButton *add = [[UIButton alloc] initWithFrame:CGRectMake(self.view.frame.size.width-50, 15, 30, 30)];
+    [add.layer setCornerRadius:15];
+    [add.layer setMasksToBounds:YES];
+    [add setBackgroundColor:[UIColor redColor]];
+    [cell addSubview:add];
+    [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+    [add addTarget:self action:@selector(addAction:) forControlEvents:UIControlEventTouchUpInside];
+    return cell;
+}
+
+- (void)addAction:(UIButton *)add{
+    
+    CGRect frame = [add convertRect:self.view.bounds toView:nil];
+    CGPoint startP = CGPointMake(frame.origin.x, frame.origin.y);
+    [[ParabolaView new] UsingFrame:add.frame startPoint:startP endPoint:_endP During:.6 Finish:^{
+        NSLog(@"抛物线动画结束");
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
