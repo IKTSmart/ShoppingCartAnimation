cfaa0d2a7243bac6b735b56c8f91ac6a62b0c4b8
IKTSmart <a5786117@qq.com>
2016-09-30 15:34:07 +0800

    addnewPro

---
+++ ShoppingCartAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingCartAnimation
+//
+//  Created by IKT on 16/9/30.
+//  Copyright © 2016年 www.tuanmob.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingCartAnimation/README.md
@@ -0,0 +1,15 @@
+# ShoppingCartAnimation
+# 一个简单添加购物车动画
+
+//调用代码
+[[ParabolaView new] UsingFrame:对象位置 startPoint:起始点 endPoint:结束点 During:时间 Finish:^{
+        NSLog(@"抛物线动画结束");
+}];
+
+## 效果
+![Alt text](https://github.com/IKTSmart/ShoppingCartAnimation/blob/master/33.gif?raw=true)
+
+
+
+## 欢迎交流
+- QQ群：<314846081>
